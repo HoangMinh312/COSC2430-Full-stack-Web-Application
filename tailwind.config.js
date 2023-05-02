022feef205801a01ca3527d08db7d70cb1611686
Tran Nguyen Anh Minh <anhminhnts2004@gmail.com>
2023-05-02 21:32:24 +0700

    commit 1st

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}", "./src/**/*.{html,js}"],
+  content: ["./*.{html,js,ejs}", "./src/**/*.{html,js,ejs}"],
   theme: {
     extend: {},
   },
