b22374e11864d4ef8a2e3daf031b7bb2b0c45333
Minh Nguyen Hoang <s3977773@rmit.edu.vn>
2023-05-04 16:19:34 +0700

    update stuff

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import express from "express";
 
 //Authentication modules
 import bcrypt from "bcrypt";
+import { router as authenticationRoute } from './src/routes/_registration.js';
 
 //Browsersync modules
 import browserSync from "browser-sync";
@@ -20,51 +21,23 @@ app.use(express.json())
 app.use(express.static("./public"))
 
 app.set('views','./src/views');
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
 
 // Testing users database
 const users = []
 
 
-app.get('/users', (req, res) => {
-    res.json(users)
-})
+
 
 // Registration routes
 
-app.post('/users', async (req, res) => {
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10)
-        const user = { username: req.body.username, password: hashedPassword}
-        users.push(user)
-        res.status(201).send()
-    } catch {
-        res.status(500).send()
-    }
-})
-
-app.post('/users/login', async (req, res) => {
-    const user = users.find(user => user.username = req.body.username)
-    if (user == null) {
-        return res.status(400).send("Cannot find user")
-    }
-
-    try {
-        if (await bcrypt.compare(req.body.password, user.password)) {
-            res.send("Success")
-        } else {
-            res.send("Not allowed")
-        }
-        
-    } catch {
-        res.status(500).send()
-    }
-})
 
 app.get("/", (req, res) => {
     console.log("Loaded website")
     res.render("index")
 })
+
+app.use("/register", authenticationRoute)
 
 app.listen(6900)
--- src/routes/_registration.js
+++ src/routes/_registration.js
@@ -1,6 +1,76 @@
 import express from 'express';
 import bcrypt from "bcrypt";
+export const router = express.Router();
 
-const router = express.Router();
+let users = []
 
-module.exports = router;
+// router.get('/', (req, res) => {
+//     res.json(users)
+// })
+
+router.get('/', (req, res) => {
+    // res.send('hello world')
+    res.send(users)
+})
+
+router.post('/', async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        const user = { username: req.body.username, password: hashedPassword}
+        users.push(user)
+        res.status(201).send()
+    } catch {
+        res.status(500).send()
+    }
+})
+
+router.post('/register/customer', async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        const user = { username: req.body.username, password: hashedPassword}
+        users.push(user)
+        res.status(201).send()
+    } catch {
+        res.status(500).send()
+    }
+})
+
+router.post('/register/shipper', async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        const user = { username: req.body.username, password: hashedPassword}
+        users.push(user)
+        res.status(201).send()
+    } catch {
+        res.status(500).send()
+    }
+})
+
+router.post('/register/vendor', async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        const user = { username: req.body.username, password: hashedPassword}
+        users.push(user)
+        res.status(201).send()
+    } catch {
+        res.status(500).send()
+    }
+})
+
+router.post('/login', async (req, res) => {
+    const user = users.find(user => user.username = req.body.username)
+    if (user == null) {
+        return res.status(400).send("Cannot find user")
+    }
+
+    try {
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            res.send("Success")
+        } else {
+            res.send("Not allowed")
+        }
+        
+    } catch {
+        res.status(500).send()
+    }
+})
