7359836a81a4eadf3795515651c04809f1130a2b
Hankaji <hankaji135@gmail.com>
2023-05-06 13:11:27 +0700

    added stuff

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ import express from "express";
 import browserSync from "browser-sync";
 import { config } from "./bs-config.js";
 const app = express();
+const port = 6900;
+
+//Routers import
+import { indexRouter }  from "./src/routes/index.js";
+import { userRouter } from "./src/routes/users.js";
 
 // BrowserSync
 const bs = browserSync.create();
@@ -10,14 +15,17 @@ bs.init({
   watch: true
 });
 
-// app
+// App setup
 app.use(express.static("./public"))
+app.use(express.urlencoded({ extended: true}));
 
 app.set('views','./src/views');
 app.set("view engine", "ejs")
 
-app.get("/", (req, res) => {
-    res.render("index")
-})
+// Routers
+app.use("/", indexRouter);
+app.use("/users", userRouter);
 
-app.listen(6900)
+app.listen(port, () => {
+  console.log(`App listening on port ${port}: http://localhost:${port}`)
+})
