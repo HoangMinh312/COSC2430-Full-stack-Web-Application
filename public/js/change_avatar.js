ba152d0d207136baa17ddafa91d4a81f9b25e914
Tran Nguyen Anh Minh <anhminhnts2004@gmail.com>
2023-05-13 09:48:05 +0700

    update changing avatar for my_account page

--- public/js/change_avatar.js
+++ public/js/change_avatar.js
@@ -1,42 +1,30 @@
-/*
-We need to register the required plugins to do image manipulation and previewing.
-*/
 FilePond.registerPlugin(
-	// encodes the file as base64 data
-  FilePondPluginFileEncode,
-	
-	// validates files based on input type
   FilePondPluginFileValidateType,
-	
-	// corrects mobile image orientation
   FilePondPluginImageExifOrientation,
-	
-	// previews the image
   FilePondPluginImagePreview,
-	
-	// crops the image to a certain aspect ratio
   FilePondPluginImageCrop,
-	
-	// resizes the image to fit a certain size
   FilePondPluginImageResize,
-	
-	// applies crop and resize information on the client
-  FilePondPluginImageTransform
+  FilePondPluginImageTransform,
+  FilePondPluginImageEdit
 );
 
-// Select the file input and use create() to turn it into a pond
-// in this example we pass properties along with the create method
-// we could have also put these on the file input element itself
+// Select the file input and use 
+// create() to turn it into a pond
 FilePond.create(
-	document.querySelector('input'),
-	{
-		labelIdle: `Drag & Drop your picture or <span class="filepond--label-action">Browse</span>`,
+  document.querySelector('input'),
+  {
+    labelIdle: `Drag & Drop your picture or <span class="filepond--label-action">Browse</span>`,
     imagePreviewHeight: 170,
     imageCropAspectRatio: '1:1',
     imageResizeTargetWidth: 200,
     imageResizeTargetHeight: 200,
     stylePanelLayout: 'compact circle',
     styleLoadIndicatorPosition: 'center bottom',
-    styleButtonRemoveItemPosition: 'center bottom'
-	}
+    styleProgressIndicatorPosition: 'right bottom',
+    styleButtonRemoveItemPosition: 'left bottom',
+    styleButtonProcessItemPosition: 'right bottom',
+  }
 );
+
+// How to use with Pintura Image Editor:
+// https://pqina.nl/pintura/docs/latest/getting-started/installation/filepond/
