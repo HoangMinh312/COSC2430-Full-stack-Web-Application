5e68d66e06cc18c87535c62b59b36da3f7a33023
Duydao28102004 <119027061+Duydao28102004@users.noreply.github.com>
2023-05-17 00:48:23 +0700

    adjust nav bar

--- src/routes/test.js
+++ src/routes/test.js
@@ -2,5 +2,5 @@ import express from "express";
 export const testRouter = express.Router();
 
 testRouter.get('/', (req, res) => {
-    res.render('vendor_page')
+    res.render('add_new_product')
 })
