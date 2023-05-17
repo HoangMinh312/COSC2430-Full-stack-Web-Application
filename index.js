31031055e19b3a16989a555b3e7b367afc9e8800
Rainb0wBanana <s3977773@rmit.edu.vn>
2023-05-17 17:21:14 +0700

    removed cart

--- index.js
+++ index.js
@@ -80,8 +80,7 @@ app.use((req, res, next) => {
     } else if (req.user instanceof Shipper) {
         userType = "Shipper"
     }
-    
-    res.session.cart = []
+
     res.locals.success_msg = req.flash('success_msg')
     res.locals.error_msg = req.flash('error_msg')
     res.locals.error = req.flash('error')
