5fef06336d199f27f629291de0ac6958b19f2922
Duydao28102004 <119027061+Duydao28102004@users.noreply.github.com>
2023-05-17 19:05:28 +0700

    respon my account, privacy and respon shopping cart

--- src/routes/test.js
+++ src/routes/test.js
@@ -2,5 +2,5 @@ import express from "express";
 export const testRouter = express.Router();
 
 testRouter.get('/', (req, res) => {
-    res.render('add_new_product')
+    res.render('privacy_policy')
 })
