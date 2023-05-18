3c0a9a54184126ed19cb3bd9a873cd28494ad7b1
Rainb0wBanana <s3977773@rmit.edu.vn>
2023-05-19 02:18:22 +0700

    Updated readme.md

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -10,6 +10,6 @@ export function ensureAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
         return next();
     }
-    req.flash('error.msg', 'Please log in to view this resource')
+    req.flash('error_msg', 'Please log in to view this resource')
     res.redirect('/auth/login')
 }
